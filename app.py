55c7b8ea159aeeafe693cf86d3577d6c6e09c77e
Hiraya025 <148345893+Hiraya025@users.noreply.github.com>
2026-04-20 21:29:16 +0800

    Initialize Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+# This is the "app" variable Vercel is looking for!
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "status": "success",
+        "message": "StockPal MVP Backend is Live!"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
